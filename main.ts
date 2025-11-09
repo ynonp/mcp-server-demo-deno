77d16e81a2e52dea70ae552eed3b0ee31fc09420
ynonp <ynonperek@gmail.com>
2025-11-09 09:00:53 +0200

    remove gui

--- main.ts
+++ main.ts
@@ -15,43 +15,6 @@ const server = new McpServer({
   },
 });
 
-const templateURI = 'ui://dadjokes/joke.html';
-
-server.registerResource(
-  'joke-widget',
-  templateURI,
-  {},
-  async () => ({
-    contents: [
-      {
-        uri: templateURI,
-        mimeType: "text/html+skybridge",
-        text: `
-        <style>
-          #dad-joke { height: 300px; }
-          p { color: green; font-size: 48px }          
-        </style>
-        <div id="dad-joke">
-          <p>Dad joke will appear here</p>
-        </div>
-        <script>
-          window.addEventListener("openai:set_globals", () => {
-            const container = document.querySelector('#dad-joke p');
-            if (openai.toolOutput && openai.toolOutput.joke) {
-              container.textContent = openai.toolOutput.joke;
-            } else {
-              container.textContent = "joke not found";
-            }          
-          });          
-        </script>
-        `,
-        _meta: {
-          "openai/widgetPrefersBorder": true,          
-        }
-      }
-    ]
-  })
-)
 
 server.registerTool(
   "tell-me-a-joke",
@@ -61,16 +24,6 @@ server.registerTool(
     },
     title: 'Joke Teller',
     description: `Tells a joke according to its index. Valid ids 0-${dadJokes.all.length - 1}`,    
-    _meta: {
-      "openai/outputTemplate": templateURI,
-      "openai/toolInvocation/invoking": "Displaying a joke",
-      "openai/toolInvocation/invoked": "Displayed a joke"
-    },
-    annotations: {
-      destructiveHint: false,
-      openWorldHint: false,
-      readOnlyHint: true,
-    }    
   },
   async ({id}) => {
     const joke = { joke: dadJokes.all[id] };
@@ -90,17 +43,7 @@ server.registerTool(
   "tell-me-a-random-joke",  
   {
     title: 'Random Joke Teller',
-    description: 'Tells a random joke',   
-    _meta: {
-      "openai/outputTemplate": templateURI,
-      "openai/toolInvocation/invoking": "Displaying a joke",
-      "openai/toolInvocation/invoked": "Displayed a joke"
-    },
-    annotations: {
-      destructiveHint: false,
-      openWorldHint: false,
-      readOnlyHint: true,
-    }         
+    description: 'Tells a random joke',    
   },
   async () => {
     const joke = {joke: dadJokes.random()};
